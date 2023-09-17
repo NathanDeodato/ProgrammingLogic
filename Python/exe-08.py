bdc0d44687a95b11f2c91af65093564d06db69fa
Nathan Deodato <65687337+NathanDeodato@users.noreply.github.com>
2023-09-17 16:20:15 -0300

    Measurement converter

--- Python/exe-08.py
+++ Python/exe-08.py
@@ -1,3 +1,19 @@
 # Converter medidas
 
-coe, viado!
+print("-" * 100)
+print("- Converter medidas -".center(100))
+print("-" * 100)
+
+medida = int(input("Metros: "))
+
+print("-" * 100)
+
+print(f"{medida}m -> {medida / 1000}km")
+print(f"{medida}m -> {medida / 100}ham")
+print(f"{medida}m -> {medida / 10}dam")
+
+print(f"{medida}m -> {medida * 10}dm")
+print(f"{medida}m -> {medida * 100}cm")
+print(f"{medida}m -> {medida * 1000}mm")
+
+
